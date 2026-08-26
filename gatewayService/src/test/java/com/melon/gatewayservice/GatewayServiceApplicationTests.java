8993b03298b5f96b3007fcf6a6071c890995320a
AB-programming <WFB15376253289@outlook.com>
2026-08-26 21:48:13 +0800

    test: add gateway module tests

--- gatewayService/src/test/java/com/melon/gatewayservice/GatewayServiceApplicationTests.java
+++ gatewayService/src/test/java/com/melon/gatewayservice/GatewayServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.melon.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
